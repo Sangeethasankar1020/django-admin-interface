ec07e8b74db2340936680c433521943fc9422b7f
Anusuya-Balakrishnan <anusuyabalakrishnan01@gmail.com>
2024-07-16 13:15:31 +0530

    form

--- Admininterfaceapp/urls.py
+++ Admininterfaceapp/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from Admininterfaceapp import views
 urlpatterns = [
-    path('',views.home,name="Home")
+    path('',views.home,name="Home"),
+    path('login/',views.login,name="Login"),
+    path('register/',views.register,name="Register")
 ]
--- Admininterfaceapp/views.py
+++ Admininterfaceapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm # to create form
+
+
+
+
+
+
 
 # Create your views here.
 def home(req):
     return render(req, "home.html")
+
+def login(req):
+    return render(req,"login.html")
+
+def register(req):
+    form=UserCreationForm()
+    return render(req,"register.html",{'form':form})
